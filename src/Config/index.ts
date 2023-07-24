bed64e5e35918f426cdf768d635123ed3951a110
unknown <apu.islam@g.bracu.ac.bd>
2023-07-25 01:29:06 +0600

    Configure project setup
    *Add mantine
    *Add folder structure
    *Setup necessary dependencies

---
+++ src/Config/index.ts
@@ -0,0 +1,1 @@
+export { customThemeConfig } from "./mantineTheme";
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
+import { customThemeConfig } from "@/Config";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider
+      theme={customThemeConfig}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
+  </React.StrictMode>
+);
